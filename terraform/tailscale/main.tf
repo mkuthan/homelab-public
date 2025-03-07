61709b8831009b5b9e87bcb1062cbd2eeaec89fb
Marcin Kuthan <marcin.kuthan@gmail.com>
2025-03-07 19:57:41 +0000

    refactor: optimize tailscale device tags resource for improved readability and maintainability

--- terraform/tailscale/main.tf
+++ terraform/tailscale/main.tf
@@ -16,11 +16,43 @@ provider "tailscale" {
 }
 
 locals {
-  proxies = [
-    "wieprz0.${var.tailscale_tailnet}",
-    "wieprz1.${var.tailscale_tailnet}",
-    "bekasow.${var.tailscale_tailnet}",
-    "vps.${var.tailscale_tailnet}"
+  machines = [
+    {
+      name = "bekasow.${var.tailscale_tailnet}"
+      tags = ["tag:server", "tag:web", "tag:samba"]
+    },
+    {
+      name = "vps.${var.tailscale_tailnet}"
+      tags = ["tag:server", "tag:web"]
+    },
+    {
+      name = "wieprz-dev.${var.tailscale_tailnet}"
+      tags = ["tag:server"]
+    },
+    {
+      name = "wieprz-pve0.${var.tailscale_tailnet}"
+      tags = ["tag:server"]
+    },
+    {
+      name = "wieprz-pve1.${var.tailscale_tailnet}"
+      tags = ["tag:server"]
+    },
+    {
+      name = "wieprz-pve2.${var.tailscale_tailnet}"
+      tags = ["tag:server"]
+    },
+    {
+      name = "wieprz-samba.${var.tailscale_tailnet}"
+      tags = ["tag:server", "tag:samba"]
+    },
+    {
+      name = "wieprz-web0.${var.tailscale_tailnet}"
+      tags = ["tag:server", "tag:web"]
+    },
+    {
+      name = "wieprz-web1.${var.tailscale_tailnet}"
+      tags = ["tag:server", "tag:web"]
+    }
   ]
 }
 
@@ -32,10 +64,10 @@ data "tailscale_devices" "devices" {
 # }
 
 resource "tailscale_device_tags" "tags" {
-  for_each = { for device in data.tailscale_devices.devices.devices : device.id => device if contains(local.proxies, device.name) }
+  for_each = { for machine in local.machines : machine.name => machine }
 
-  device_id = each.key
-  tags      = ["tag:proxy"]
+  device_id = lookup({for d in data.tailscale_devices.devices.devices : d.name => d.id}, each.key, null)
+  tags      = each.value.tags
 }
 
 data "tailscale_users" "owners" {
